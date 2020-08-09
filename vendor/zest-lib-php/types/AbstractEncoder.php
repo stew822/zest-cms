25eb45f4bcd2ecfe32de1d4eb3a85324460c9a15
Stewart B <stew822@gmail.com>
2020-08-09 03:34:12 +0000

    removed zest-lib-php, added gitignore

--- vendor/zest-lib-php/types/AbstractEncoder.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace zest\types;
-
-abstract class AbstractEncoder {
-    abstract public function create( $location );
-    abstract public function serialise( $value, $location ); 
-    abstract public function deserialise( $location ); 
-}
--- vendor/zest-lib-php/types/AbstractType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace zest\types;
-
-abstract class AbstractType {
-    abstract public function name();
-    abstract public function extension();
-    abstract public function encoder();
-}
